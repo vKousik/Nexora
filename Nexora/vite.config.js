96955810eb6ebe4b4de95b7382749b01b5be7766
Abirpaul007 <abirpaulbkp@gmail.com>
2025-08-20 16:54:11 +0530

    update

--- Nexora/vite.config.js
+++ Nexora/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   build: {
+     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks: {
